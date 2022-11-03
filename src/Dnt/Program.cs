72a102753caea8626fd477aaab000bf7a97f382a
Kondratov Konstantin <kkondratov@renins.com>
2022-11-03 15:02:07 +0300

    Using visualstudio msbuild instance by default

--- src/Dnt/Program.cs
+++ src/Dnt/Program.cs
@@ -26,8 +26,32 @@ namespace Dnt
             {
                 // MSBuildLocator takes care of finding the default MSBuild / Visual Studio instance
                 // and setting up all necessary environment variables and paths for MSBuild.
-                MSBuildLocator.RegisterDefaults();
+                var instanses = MSBuildLocator.QueryVisualStudioInstances(new VisualStudioInstanceQueryOptions
+                {
+                    DiscoveryTypes = DiscoveryType.DeveloperConsole | DiscoveryType.VisualStudioSetup | DiscoveryType.DotNetSdk
+                });
+                
+                var iii = instanses.ToList();
+                
+#if NETCOREAPP3_1_OR_GREATER
+                var x = BlackFox.VsWhere.VsInstances.GetAll().OrderByDescending(x => new Version(x.InstallationVersion)).First();
 
+                var a = (VisualStudioInstance)Activator
+                    .CreateInstance(
+                        typeof(VisualStudioInstance),
+                        BindingFlags.NonPublic | BindingFlags.Instance,
+                        null,
+                        new object[]
+                        {
+                            x.DisplayName,
+                            x.InstallationPath,
+                            new Version(x.InstallationVersion),
+                            DiscoveryType.VisualStudioSetup
+                        },
+                        culture: null);
+                iii.Add(a);
+#endif
+                MSBuildLocator.RegisterInstance(iii.Find(x => x.DiscoveryType == DiscoveryType.VisualStudioSetup));
                 var processor = new CommandLineProcessor(new CoreConsoleHost());
                 processor.RegisterCommandsFromAssembly(typeof(CommandBase).Assembly);
                 processor.Process(args);
